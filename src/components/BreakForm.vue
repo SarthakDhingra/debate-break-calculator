3760dc3ea304f2cc19e944ec31d104c5e509f200
Sarthak Dhingra <sdhingra@ualberta.ca>
2021-06-29 22:20:16 -0600

    added bulma styling

--- src/components/BreakForm.vue
+++ src/components/BreakForm.vue
@@ -1,31 +1,40 @@
 <template>
-    <div class="child">
-        <form class="pure-form pure-form-stacked splash-subhead" @submit.prevent="onSubmit($event)">
-            <label for="teams">TEAMS</label>
-            <input id="teams" v-model.number="teams">
-            <br>
-            <label for="rounds">ROUNDS</label>
-            <input id="rounds" v-model.number="rounds">
-            <br>
-            <label for="breaking">BREAKING</label> 
-            <input id="breaking" v-model.number="breaking">
-            <br>
-            <button class="pure-button" name="style" value=2 type="submit">Two Teams</button>
-            <button class="pure-button" name="style" value=4 type="submit">Four Teams</button> 
-        </form>
+    <div class="columns has-text-centered" >
+        <div class="column"></div>
+        <div class="column">
+            <div class="card">
+                <div class="card-content">
+                    <div class="content">
+                        <form class="pure-form pure-form-stacked splash-subhead" @submit.prevent="onSubmit($event)">
+                            <label class="label" for="teams">TEAMS</label>
+                            <input class="input" id="teams" v-model.number="teams">
+                            <br>
+                            <label class="label" for="rounds">ROUNDS</label>
+                            <input class="input" id="rounds" v-model.number="rounds">
+                            <br>
+                            <label class="label" for="breaking">BREAKING</label> 
+                            <input class="input" id="breaking" v-model.number="breaking">
+                            <br>
+                            <br>
+                            <button class="button" name="style" value=2 type="submit">Two Teams</button>
+                            <button class="button" name="style" value=4 type="submit">Four Teams</button> 
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column"></div>
     </div>
 
-    <div v-if="displayResults" class="splash-subhead grandchild">
-        <table>
-            <tr>
-                <th>BEST CASE</th>
-                <th>WORST CASE</th>
-            </tr>
-            <tr>
-                <td>{{best_string}}</td>
-                <td>{{worst_string}}</td>
-            </tr>
-        </table>
+    <div v-if="displayResults">
+        <div class="columns is-mobile has-text-centered">
+            <p class="column">Best Case</p>
+            <p class="column">Worst Case</p>
+        </div>
+        <div class="columns is-mobile has-text-centered">
+            <p class="column">{{best_string}}</p>
+            <p class="column">{{worst_string}}</p>
+        </div>
     </div>
 </template>
 
@@ -48,18 +57,12 @@ export default {
     methods: {
         onSubmit(event) {
 
-            console.log(event)
-
             let style = parseInt(event.submitter.attributes.value.value)
             let tournament = new Tournament(style)
             let results = tournament.getBreak(this.teams, this.breaking, this.rounds)
 
-            console.log("RESULTS")
             let best = results[0]
             let worst = results[1]
-            console.log(best)
-            console.log(worst)
-            console.log(event)
 
             this.best_string = `All teams on ${best.guranteed_break} points will break. ${best.breaking_on_speaks} out of ${best.total_on_speaks} teams on ${best.speaks_break} points will break`
             this.worst_string = `All teams on ${worst.guranteed_break} points will break. ${worst.breaking_on_speaks} out of ${worst.total_on_speaks} teams on ${worst.speaks_break} points will break`
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="has-text-centered">
+        <h1 class="title is-1">{{title}}</h1>
+        <h5 class="subtitle is-5">{{description}}</h5>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Header",
+    data() {
+        return {
+            title: "Debate Breaker",
+            description: "Determine the points needed to advance to outrounds at debating tournaments"
+        }
+    }
+}
+</script>
